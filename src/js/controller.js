874f0f540a7cce9d1c132978b96753867aadeaf7
Halit Hamza Şeker <shalithamza@gmail.com>
2024-03-30 18:58:23 +0300

    lesson 2 complete

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,7 +1,7 @@
 import icons from "url:../img/icons.svg";
 
-import "core-js/stable"
-import "regenerator-runtime/runtime"
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 
 const recipeContainer = document.querySelector(".recipe");
 
